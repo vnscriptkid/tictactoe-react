2a4ede7d5b61fe53ea85d781b3bb6a811abed285
unknown <vnscriptkid@gmail.com>
2018-04-17 16:42:42 +0700

    my first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>Play game</div>, document.getElementById('root'));
+registerServiceWorker();
